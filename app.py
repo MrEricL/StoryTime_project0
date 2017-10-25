cd146d7a1b68005c0d91a965c01ff9cf554182cd
MrEricL <eli6@stuy.edu>
2017-10-25 14:06:06 -0400

    thingy

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template, request, session, url_for, flash
+from flask import Flask, render_template, request, session, url_for, flash, redirect
 import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(32)  #for the cookies
+
+logged = True
+#need a method to update this
 
 @app.route('/')
 def root():
     #redirect to home if there is a session
     #otherwise display login/register page 
+    return redirect(url_for("login"))
 
 #authenticate user credentials
 @app.route('/login')
@@ -14,30 +19,56 @@ def login():
     #if successful, redirect to home
     #otherwise redirect back to root?
 
-#create new account 
-@app.route('/register')
-def register():
-    #if there isn't an existing user w/ same username, add to users table 
-    #otherwise redirect to root with flashed message 
-    
-#log out user
-@app.route('/logout/')
-def logout():
-    #pop out user from session
-    #flash logout message
-    #redirect to root
+    #if <logged in>: 
+    #   return redirect(url_for("home"))
+    if logged:
+        return redirect("home")
+    else:
+        return render_template("login.html")
+
 
 #user dashboard 
 @app.route('/home')
 def home():
     #render template for dashboard
+    if logged:
+        return render_template("home.html")
+    else:    
+        return redirect(url_for("login"))
+        
 
 #allows user to view stories/add to stories (unless we want to separate the two)
 @app.route('/view')
 def view():
     #render template
+    if logged:
+        return render_template("addview.html")
+    else:    
+        return redirect(url_for("login"))
 
 #allows user to create new story 
 @app.route('/newstory')
 def new():
     #render template
+    if logged:
+        return render_template("new.html")
+    else:    
+        return redirect(url_for("login"))
+if __name__ == '__main__':
+    app.run(debug=True)
+
+''' JUNK PILE
+#create new account 
+@app.route('/register')
+def register():
+    #if there isn't an existing user w/ same username, add to users table 
+    #otherwise redirect to root with flashed message 
+
+
+#log out user
+@app.route('/logout')
+def logout():
+    #pop out user from session
+    #flash logout message
+    #redirect to root
+    '''
