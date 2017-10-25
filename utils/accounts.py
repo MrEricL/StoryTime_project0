3fec7a79ec475ec7f731227a31b9d8f4a8fe0cf5
angelng1412 <ang7@stuy.edu>
2017-10-25 13:36:30 -0400

    rando

--- utils/accounts.py
+++ utils/accounts.py
@@ -1,3 +1,4 @@
+import sqlite3
 from db_builder import getInfo, addUser
 
 BAD_USER = -1
@@ -5,14 +6,20 @@ BAD_PASS = -2
 GOOD = 1
 
 def authenticate(user, passw):
+     f = "usersandstories.db"
+     db = sqlite3.connect(f)
+     c = db.cursor()
+     
      info = getInfo(user)
+     print info
      if info == None:
          return BAD_USER
      elif info != passw:
-         return BAD_PASS
+          return BAD_PASS
      else:
-         return GOOD
-
+          return GOOD
+     db.close()
+     
 print authenticate('manahal', 'mt123')
 print authenticate('joe', 'mt123')
 print authenticate('jack', 'mt123')
@@ -20,3 +27,4 @@ print authenticate('jack', 'mt123')
 
 def register(user, passw):
      addUser(user,passw)
+
